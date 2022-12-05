aecc98e41a81ad934c932f1f5f282a3043197c12
I-Maged <magedhanafy@gmail.com>
2022-12-05 14:49:47 +0200

    form styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className='flex flex-col justify-between items-center h-screen font-mono'>
       <Header />
-      <h1 className='text-4xl'>
-        Garlic bread with cheese: What the science tells us
-      </h1>
-      <article className='container'>
-        <p className='text-primary'>
-          For years parents have espoused the health benefits of eating garlic
-          bread with cheese to their children, with the food earning such an
-          iconic status in our culture that kids will often dress up as warm,
-          cheesy loaf for Halloween.
-        </p>
-        <p>
-          But a recent study shows that the celebrated appetizer may be linked
-          to a series of rabies cases springing up around the country.
-        </p>
-      </article>
+      <main className='container flex-col h-screen'>
+        <Form />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+function Form() {
+  return (
+    <form className='flex justify-center my-12'>
+      <input
+        type='text'
+        placeholder='Add Todo'
+        className='input input-bordered input-primary mr-3'
+      />
+      <button className='btn btn-primary'>Submit</button>
+    </form>
+  );
+}
+
+export default Form;
